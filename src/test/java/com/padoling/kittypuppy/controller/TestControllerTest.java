d9d0d16392bfca2991cbda88c3b377b7ef823df5
padoling <hysm920@gmail.com>
2020-05-29 17:44:24 +0900

    [add] test files, travis branch list

---
+++ src/test/java/com/padoling/kittypuppy/controller/TestControllerTest.java
@@ -0,0 +1,13 @@
+package com.padoling.kittypuppy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestControllerTest {
+
+    @Test
+    public void test() {
+
+    }
+}
