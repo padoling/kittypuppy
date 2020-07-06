b972d4bd342235e70e4fb707c3bf4df8d2f24e27
padoling <hysm920@gmail.com>
2020-07-06 16:36:18 +0900

    [modify] test code 수정 :
    test용 h2 database 추가
    mockMvc 이용한 web layer test

--- src/test/java/com/padoling/kittypuppy/controller/TestControllerTest.java
+++ src/test/java/com/padoling/kittypuppy/controller/TestControllerTest.java
@@ -1,13 +1,29 @@
 package com.padoling.kittypuppy.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TestControllerTest {
 
-    @Test
-    public void test() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void test() throws Exception {
+        String profile = "Profile";
+
+        this.mockMvc.perform(get("/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(profile)));
     }
 }
