ce191e006c3d0b53b458fe0a2227b8fd76ff59c6
padoling <hysm920@gmail.com>
2020-07-09 17:31:50 +0900

    [add] Spring Rest Docs 적용

--- src/main/java/com/padoling/kittypuppy/controller/TestController.java
+++ src/main/java/com/padoling/kittypuppy/controller/TestController.java
@@ -5,15 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping
+@RequestMapping("/test")
 public class TestController {
 
     @Value("${profile}")
     private String profile;
 
-    @GetMapping("/test")
-    public String testIndex() {
-        return "Profile : " + this.profile;
+    @GetMapping
+    public Map<String, String> testIndex() {
+        Map<String, String> map = new HashMap<>();
+        map.put("profile", profile);
+        return map;
     }
 }
--- src/test/java/com/padoling/kittypuppy/controller/TestControllerTest.java
+++ src/test/java/com/padoling/kittypuppy/controller/TestControllerTest.java
@@ -1,18 +1,25 @@
 package com.padoling.kittypuppy.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@WebMvcTest(TestController.class)
+@AutoConfigureRestDocs
 public class TestControllerTest {
 
     @Autowired
@@ -20,10 +27,17 @@ public class TestControllerTest {
 
     @Test
     public void test() throws Exception {
-        String profile = "Profile";
+        //given
+        String profile = "profile";
 
+        //then
         this.mockMvc.perform(get("/test"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString(profile)));
+                .andExpect(content().string(containsString(profile)))
+                .andDo(document("test-get",
+                        responseFields(
+                                fieldWithPath("profile").type(JsonFieldType.STRING).description("현재 구동 중인 프로필")
+                        )
+                ));
     }
 }
